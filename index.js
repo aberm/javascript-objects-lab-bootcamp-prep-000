d455ca13c29a8d61f7136455c98e4ccf57762cf3
Menachem Aber <abermendel@gmail.com>
2018-11-08 19:21:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var object2 = Object.assign
-  return object
+  var object2 = Object.assign({}, object)
+  object2[key] = value
+  return object2
 }
